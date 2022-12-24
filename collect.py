25dc2143e9dbc855f1571a06f679ccca33bd2756
lawn <jaebeomseol@gmail.com>
2022-12-24 15:48:40 +0900

    collect 관련 코드 추가

---
+++ collect.py
@@ -0,0 +1,14 @@
+from utility.botinfo import BotInfo
+from utility.database import Database
+
+if __name__ == "__main__":
+    botinfo = BotInfo("conf/botinfo.cnf")
+    database = Database("data/info.sql3")
+    rank_json = botinfo.getKoreanBotByHeartRankList(1)
+    rank_list = rank_json['data']['data']
+    for no in range(len(rank_list)):
+        is_exist_count = len(database.selectBotInfo(rank_list[no]['id']))
+        if (is_exist_count == 0):
+            database.insertKoreanBotInfo(rank_list[no])
+    bot_list = database.selectBotList("뮤직")
+    print(bot_list)
